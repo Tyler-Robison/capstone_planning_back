5b7cc65291ec682a149f66358b6af345157078c0
Tyler Robison <tylerobison758758@gmail.com>
2022-02-22 21:22:23 -0500

    changed 1 file

---
+++ secret.js
@@ -0,0 +1,7 @@
+// File for storing API KEYS
+// Different from SECRET_KEY used for token creation in config.js
+// Not associated with a payment method, don't put in git regardless
+
+const SPOON_API_KEY = 'e11de31554674e8f9eb207f54aab2a27';
+
+module.exports = SPOON_API_KEY;
